ccc94f1f9b1740d1ce85b994326955c1e67bd644
dskman <218834576+dskman1@users.noreply.github.com>
2026-04-05 23:46:16 +0200

    - Removed extra repetitiveness inside playGame(playRound())with a recursive call, more compact (else if) for all computerChoices - Added a default case for getHumanChoice() - Removed globla varibales

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-function getComputerChoise(){
+function getComputerChoice(){
     let randomNum = Math.floor(Math.random() * 3) + 1;
     return randomNum == 1 ? "rock" : 
     randomNum == 2 ? "paper" : "scissors";
 }
 
 function getHumanChoice(){
-    let input = prompt("Enter your choice");
+   let input = prompt("Enter your choice (Rock , Paper , Scissors)");
     let choice = input.toLowerCase();
-    return choice == "rock" ? "rock" : 
-    choice == "scissors" ? "scissors" : "paper";
+    if(choice === "rock" 
+    || choice === "paper" 
+    || choice === "scissors"){
+        return choice;
+    } else {
+        console.log("No choice! Default PAPER")
+        return "paper";
+    }
 }
 
-let humanScore = 0 
-let computerScore = 0;
 function playGame(){
-    function playRound(humanChoice, computerChoice){
+    let humanScore = 0 
+    let computerScore = 0;
+    let rounds = 0;
+
+    function playRound(){
+        if(rounds == 5)return;
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+
         if(computerChoice == humanChoice){
             console.log("It's A Tie");
         }
-        else if(computerChoice == "paper" && humanChoice == "rock"){
+        else if(computerChoice == "paper" && humanChoice == "rock" 
+            || computerChoice =="scissors" && humanChoice == "paper" 
+            || computerChoice == "rock" && humanChoice == "scissors"){
             console.log("Computer Wins!");
             computerScore++;
-        }
-        else if(computerChoice =="scissors" && humanChoice == "paper"){
-            console.log("Computer Wins!");
-            computerScore++;
-        }
-        else if(computerChoice == "rock" && humanChoice == "scissors"){
-            console.log("Computer Wins!")
-            computerScore++;
-        }
-
-        else {
-
+        } else {
             console.log("You Win!")
             humanScore++;
         }
+        rounds++;
+        playRound();
     }
-    
-playRound(getHumanChoice(), getComputerChoise());
-playRound(getHumanChoice(), getComputerChoise());
-playRound(getHumanChoice(), getComputerChoise());
-playRound(getHumanChoice(), getComputerChoise());
-playRound(getHumanChoice(), getComputerChoise());
+    playRound();
 
-
+    console.log(`Human: ${humanScore}, Computer: ${computerScore}, Rounds: ${rounds}`);
 }
 
 playGame();
-console.log(`Human: ${humanScore}, Computer: ${computerScore}`);
